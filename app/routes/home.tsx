fe122d9c8ea24feb17865af000f5524f4ede5999
zena <zena@carmon.co.kr>
2025-05-19 23:21:46 +0900

    🚧 WIP

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
+import { Welcome } from "../welcome/welcome"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: "임송하 - 포트폴리오" },
+    { name: "description", content: "Frond-end Developer Portfoilo" },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Welcome />
 }
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,13 +1,14 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
-
+import { MdOutlineMail } from "react-icons/md"
+import { MdOutlinePhoneAndroid } from "react-icons/md"
+import { FaGithub } from "react-icons/fa"
+import img from "./images/img_me.png"
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
+    <main className='flex items-center justify-center pt-16 pb-4'>
+      <div className='flex-1 flex flex-col items-center min-h-0'>
+        <header className='flex flex-col items-center'>
+          <div className='w-[866px] max-w-[100vw]'>
+            {/* <img
               src={logoLight}
               alt="React Router"
               className="block w-full dark:hidden"
@@ -16,74 +17,113 @@ export function Welcome() {
               src={logoDark}
               alt="React Router"
               className="hidden w-full dark:block"
-            />
+            /> */}
+            <div className='text-[20px]'>Front-end Developer</div>
+            <div className='text-[64px] font-bold'>Portfolio</div>
+            {/* <div className='px-3 py-2 border w-36 text-center rounded-full mt-10'>
+              임송하
+            </div> */}
+            <div className='border border-b-0 mt-1'></div>
           </div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
+        <div className='max-w-[866px] w-full space-y-6'>
+          <nav className='mt-10 space-y-4'>
+            <div className='w-60'>
+              <img src={img} alt='' className='rounded-2xl' />
+            </div>
+            <p className='leading-6 text-2xl text-indigo-600'>Contact</p>
             <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
+              {resources.map(({ subText, text, icon }) => (
+                <li key={subText}>
+                  <div
+                    className='group flex items-center gap-2 leading-normal'
+                    // href={href}
+                    // target='_blank'
+                    // rel='noreferrer'
                   >
                     {icon}
-                    {text}
-                  </a>
+                    <span>{subText}</span>
+                  </div>
                 </li>
               ))}
             </ul>
+
+            <div className='text-2xl text-indigo-600'>Introduce</div>
+            <div className='px-2 grid gap-3 leading-7'>
+              <div>
+                저는 단순히 기능을 구현하는 데 그치지 않고, 제가 만든 서비스를
+                실제 사용자가 경험하고, 피드백을 주고, 그에 따라 빠르게 개선해
+                나가는 반복의 사이클에서 큰 동기와 재미를 느낍니다.
+              </div>
+              <div>
+                개발을 하며 무엇보다 중요한 건 '사용자 입장에서 불편함 없이
+                사용할 수 있는가'라고 생각하기 때문에 이런 관점을 중심에 두고,
+                더 명확하고 직관적인 화면을 고민하며, 때로는 개발보다 사용자
+                경험 설계를 우선하기도 합니다.
+              </div>
+              <div>
+                새로운 기술 스택이나 팀 환경, 낯선 도메인에 빠르게 적응하고,
+                실질적인 결과물로 연결시키는 실행력이 제 강점이며, 익숙하지 않은
+                기술이나 문제에 부딪혀도 구조를 빠르게 파악하고, 필요한 방향을
+                주도적으로 설정해 나가는 데 자신 있습니다.
+              </div>
+              <div>
+                앞으로 저는 변화에 빠르게 적응하고, 사용자 경험에 최적화된
+                개발을 통해 사람들에게 실질적인 가치를 전달할 수 있는 프론트엔드
+                개발자로 계속 성장해가고자 합니다.
+              </div>
+            </div>
+
+            <div className='text-2xl text-indigo-600'>Skills</div>
+            <div className='px-2'>
+              <div>
+                🛠 React, Next, React Native, TypeScript, JavaScript, Tailwind
+                CSS, HTML, CSS, SCSS
+              </div>
+              <div>🖇 Git, Git-flow, Jira, Figma</div>
+            </div>
           </nav>
         </div>
       </div>
     </main>
-  );
+  )
 }
 
 const resources = [
   {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
+    text: "Mobile",
+    subText: "010-3109-3499",
+    icon: <MdOutlinePhoneAndroid />,
   },
   {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
+    text: "E-mail",
+    subText: "dddtttt000@gmail.com",
+    icon: <MdOutlineMail />,
   },
-];
+  {
+    text: "Github",
+    subText: "github.com/dddtttt000",
+    icon: <FaGithub />,
+  },
+  // {
+  //   text: "Blog",
+  //   subText: ": notion.site/Songha",
+  //   href: "https://www.notion.so/TIL-41737a9151154ba99e9d2c8e6940a058?pvs=21",
+  //   icon: (
+  //     <svg
+  //       xmlns='http://www.w3.org/2000/svg'
+  //       width='24'
+  //       height='20'
+  //       viewBox='0 0 20 20'
+  //       fill='none'
+  //       className='stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300'
+  //     >
+  //       <path
+  //         d='M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z'
+  //         strokeWidth='1.5'
+  //         strokeLinecap='round'
+  //       />
+  //     </svg>
+  //   ),
+  // },
+]
